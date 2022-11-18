36f673a0df2a97e396de70088d693baec800d83e
admin <554286238@qq.com>
2022-11-18 16:57:09 +0800

    安装脚本

--- script/部署exporter/exporter_install.sh
+++ script/部署exporter/exporter_install.sh
@@ -25,7 +25,31 @@ do
     sudo chown -R $RUN_USER:$RUN_USER_GROUP ./$EXPORTER_TAR_EXT_NAME
  fi
 
- sudo -u $RUN_USER nohup ./$EXPORTER_TAR_EXT_NAME/mongodb_exporter --mongodb.uri=mongodb://$MONOGO_MONITOR_USER:$MONOGO_MONITOR_USER_PWD@localhost:$MONOGO_PORT --web.listen-address=:$EXPORTER_PORT --collector.dbstats --collector.topmetrics --collector.diagnosticdata --collector.replicasetstatus --collector.indexstats --collector.collstats --collect-all --compatible-mode &
+ echo '开始部署export自启动服务'
+   echo '[Unit]
+Description=v2 Mongodb Exporter
+After=local-fs.target network-online.target network.target
+Wants=local-fs.target network-online.target network.target
+
+[Service]
+User='${RUN_USER}'
+Group='${RUN_USER_GROUP}'
+Type=simple
+ExecStart='${RUN_PATH}'/'$EXPORTER_TAR_EXT_NAME'/mongodb_exporter --mongodb.uri=mongodb://'$MONOGO_MONITOR_USER':'$MONOGO_MONITOR_USER_PWD'@localhost:'$MONOGO_PORT' --web.listen-address=:'$EXPORTER_PORT' --collector.dbstats --collector.topmetrics --collector.diagnosticdata --collector.replicasetstatus --collector.indexstats --collector.collstats --collect-all --compatible-mode
+Restart=on-failure
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/v2_mongodb_export_${MONOGO_PORT}.service
+
+systemctl daemon-reload
+systemctl enable v2_mongodb_export_${MONOGO_PORT}
+
+systemctl start v2_mongodb_export_${MONOGO_PORT}
+
+
+
+# sudo -u $RUN_USER nohup ./$EXPORTER_TAR_EXT_NAME/mongodb_exporter --mongodb.uri=mongodb://$MONOGO_MONITOR_USER:$MONOGO_MONITOR_USER_PWD@localhost:$MONOGO_PORT --web.listen-address=:$EXPORTER_PORT --collector.dbstats --collector.topmetrics --collector.diagnosticdata --collector.replicasetstatus --collector.indexstats --collector.collstats --collect-all --compatible-mode &
   echo 'mongodb端口'$MONOGO_PORT'监控exporter部署完毕，exporter对外地址为'$ip':'$EXPORTER_PORT
 
 done < $INSTALL_CONFIG
